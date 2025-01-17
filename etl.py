688a18fd22e4c8f22a7783a62e87690c7d09c492
Victor Almeida <vrodrigoleite@gmail.com>
2025-01-17 16:28:08 -0300

    Pipeline de carregamento de dados em csv e parquet

--- etl.py
+++ etl.py
@@ -4,7 +4,7 @@ import glob
 
 # Ler um arquivo
 
-def extrair_dados(pasta: str) -> pd.DataFrame:
+def extrair_dados_e_consolidar(pasta: str) -> pd.DataFrame:
 
     arquivos_json = glob.glob(os.path.join(pasta, '*.json'))
     df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
@@ -12,14 +12,26 @@ def extrair_dados(pasta: str) -> pd.DataFrame:
 
     return df_total
 
+# Adicionar o kpi
 
-# Concatenar um arquivo
+def calcular_kpi_de_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
 
-# Transformar os arquivos
+    df['Total'] = df['Quantidade'] * df['Venda']
+
+    return df
 
 # Load dos meus arquivos em csv ou parquet
 
-# Quando for testar as coisas, testar dessa forma. Pois caso esqueça de apagar, não danifica o código
-if __name__ == '__main__':
-    pasta = 'data'
-    print(extrair_dados(pasta=pasta))
+def carregar_dados(df: pd.DataFrame, fomarto_saida: list):
+    for formato in fomarto_saida:
+        if formato == 'csv':
+            df.to_csv('dados.csv',index= False)
+        if formato == 'parquet':
+            df.to_csv('dados.parquet',index= False)
+
+def pipeline(pasta: str, formato_saida: list):
+
+    df_consolidado = extrair_dados_e_consolidar(pasta)
+    df_consolidado_com_kpi = calcular_kpi_de_total_de_vendas(df_consolidado)
+
+    carregar_dados(df_consolidado_com_kpi, formato_saida)
--- pipeline.py
+++ pipeline.py
@@ -1,1 +1,6 @@
-pasta = 'data'
+from etl import pipeline   
+
+pasta: str = 'data'
+formato_saida: list = ['csv', 'parquet']
+
+pipeline(pasta, formato_saida)
